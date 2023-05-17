c98c6e4b14612c9304b0e08b9e058c25fe9eabdb
juyong31 <actyoung31@naver.com>
2023-05-17 21:29:16 +0900

    로그인/회원가입

--- ForTest/UBee/app/src/main/java/com/example/ubee/LoginActivity.kt
+++ ForTest/UBee/app/src/main/java/com/example/ubee/LoginActivity.kt
@@ -1,10 +1,19 @@
 package com.example.ubee
 
 import android.content.Intent
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import androidx.databinding.DataBindingUtil
+import com.example.ubee.MyApplication.Companion.auth
 import com.example.ubee.databinding.ActivityAnnounceBinding
 import com.example.ubee.databinding.ActivityLoginBinding
+import com.example.ubee.databinding.FragmentHomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
 
@@ -13,15 +22,29 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         binding = ActivityLoginBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
-        binding.signUpBtn.setOnClickListener{
+        binding.signUpBtn.setOnClickListener {
             val intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+
+        binding.loginBtn.setOnClickListener {
+            val email = binding.authEmailEditView.text.toString()
+            val password = binding.authPasswordEditView.text.toString()
+            MyApplication.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this, "로그인 성공", Toast.LENGTH_LONG).show()
+                        val intent = Intent(this, HomeFragment::class.java)
+                        startActivity(intent)
+                    }else {
+                        Toast.makeText(this, "로그인 실패", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
 
     }
 }
--- ForTest/UBee/app/src/main/java/com/example/ubee/SignupActivity.kt
+++ ForTest/UBee/app/src/main/java/com/example/ubee/SignupActivity.kt
@@ -1,24 +1,58 @@
 package com.example.ubee
 
 import android.content.Intent
+import android.os.Build.VERSION_CODES.M
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.navigation.fragment.findNavController
 import com.example.ubee.databinding.ActivitySignupBinding
 import androidx.navigation.fragment.findNavController
+import com.example.ubee.MyApplication.Companion.auth
+import com.example.ubee.databinding.ActivityLoginBinding
+import com.example.ubee.databinding.FragmentHomeBinding
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignupBinding
+    lateinit var binding: ActivitySignupBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         binding = ActivitySignupBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+        //여기서부터 수정
+
+        binding.goSignInBtn.setOnClickListener {
+            val email = binding.authEmailEditView.text.toString()
+            val password = binding.authPasswordEditView.text.toString()
+            MyApplication.auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if(task.isSuccessful){
+                        MyApplication.auth.currentUser?.sendEmailVerification()
+                            ?.addOnCompleteListener { sendTask ->
+                                if(sendTask.isSuccessful){
+                                    Toast.makeText(baseContext, "회원가입에서 성공, 전송된 메일을 확인해 주세요", Toast.LENGTH_SHORT).show()
+                                    val intent = Intent(this, LoginActivity::class.java)
+                                    startActivity(intent)
+                                }else {
+                                    Toast.makeText(baseContext, "메일 발송 실패", Toast.LENGTH_SHORT).show()
+                                    val intent = Intent(this, LoginActivity::class.java)
+                                    startActivity(intent)
+                                }
+                            }
+                    }else {
+                        Toast.makeText(baseContext, "회원가입 실패", Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
+                }
+            }
 
         // 툴바 세팅 (https://hamzzibari.tistory.com/102)
         val toolbar = binding.toolbarMap
@@ -26,7 +60,11 @@ class SignupActivity : AppCompatActivity() {
         val ab = supportActionBar!!
         ab.setDisplayShowTitleEnabled(false)
         ab.setDisplayHomeAsUpEnabled(true)
+
+
+
     }
+
 
     // 뒤로가기 동작 (https://hamzzibari.tistory.com/102)
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
