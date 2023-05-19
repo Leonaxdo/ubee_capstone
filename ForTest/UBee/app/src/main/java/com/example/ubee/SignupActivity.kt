4a532738b85710184bfbbef02fcc635b51cdab8f
juyong31 <actyoung31@naver.com>
2023-05-19 21:34:55 +0900

    회원가입 데이터 저장
    파이어스토어를 통해 회원가입시 작성되는 데이터를 저장

--- ForTest/UBee/app/src/main/java/com/example/ubee/SignupActivity.kt
+++ ForTest/UBee/app/src/main/java/com/example/ubee/SignupActivity.kt
@@ -12,6 +12,7 @@ import androidx.navigation.fragment.findNavController
 import com.example.ubee.databinding.ActivitySignupBinding
 import androidx.navigation.fragment.findNavController
 import com.example.ubee.MyApplication.Companion.auth
+import com.example.ubee.MyApplication.Companion.email
 import com.example.ubee.databinding.ActivityLoginBinding
 import com.example.ubee.databinding.FragmentHomeBinding
 import com.google.firebase.auth.ktx.auth
@@ -34,6 +35,7 @@ class SignupActivity : AppCompatActivity() {
             MyApplication.auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this) { task ->
                     if(task.isSuccessful){
+                        saveStore()
                         MyApplication.auth.currentUser?.sendEmailVerification()
                             ?.addOnCompleteListener { sendTask ->
                                 if(sendTask.isSuccessful){
@@ -75,5 +77,18 @@ class SignupActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+
+    private fun saveStore(){
+        val data = mapOf(
+            "email" to binding.authEmailEditView.text.toString(),
+            "name" to binding.editTextTextPersonName.text.toString(),
+            "identityNum" to binding.editTextTextPersonName3.text.toString()
+        )
+
+        MyApplication.db.collection("userData")
+            .document(binding.editTextTextPersonName.text.toString())
+            .set(data)
     }
 }
